8c9c65a70b275de20e2babc48095ea93be3910f8
Manoj Kumar Munda <manojkrmunda828@gmail.com>
2024-05-09 11:52:01 +0530

    bugs fixed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -27,9 +27,8 @@ const Carousel = () => {
     <div className="flex flex-col gap-y-4">
       <div className=" relative w-60 overflow-hidden">
         <div
-          className={`relative flex transition-all duration-300 -translate-x-${(
-            activeIndex * 100
-          ).toString()}`}
+          style={{ left: `-${activeIndex*100}%`}}
+          className={`relative flex transition-all duration-300`}
         >
           {heroTexts.map((item, index) => (
             <div className="shrink-0 w-full my-4" key={item}>
